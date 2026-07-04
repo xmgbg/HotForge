80cdd183e6c9efaae44a809b5be5b31ffe869ee3
小马鸽 <2691613199@qq.com>
2026-07-04 17:51:31 +0800

    初始化 HotForge 项目骨架

---
+++ src/main/java/org/example/hotforge/HotForgeApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hotforge/HotForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
